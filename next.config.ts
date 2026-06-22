426310a79b6eccafbce253f26e19475e2360fe84
SachdevaKriti <kriti7272@gmail.com>
2026-06-22 11:53:49 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'crests.football-data.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
